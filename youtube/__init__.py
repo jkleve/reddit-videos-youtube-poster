cec80829411f8ec5048e42ef6a2b1e2c218762ba
Jesse Kleve <jesse.kleve@gmail.com>
2019-01-13 21:15:04 -0700

    fixed youtube regex for odd cases

--- youtube/__init__.py
+++ youtube/__init__.py
@@ -15,5 +15,5 @@ __all__ = [
 ]
 
 
-video_id_regex = re.compile('watch\?v=([a-zA-Z0-9-_]+)')
+video_id_regex = re.compile('watch\?.*v=([a-zA-Z0-9-_]+)')
 video_id_regex_short = re.compile('youtu\.be/([a-zA-Z0-9-_]+)')
