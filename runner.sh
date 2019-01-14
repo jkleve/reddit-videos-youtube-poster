75c28d184cb27967f67b15402edc83d49ff7b593
Jesse Kleve <jesse.kleve@gmail.com>
2019-01-13 21:27:11 -0700

    changed daemon script to redirect to log file

--- runner.sh
+++ runner.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# https://serverfault.com/questions/103501/how-can-i-fully-log-all-bash-scripts-actions
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+
 if [ -f venv/bin/activate ]; then
     source venv/bin/activate
 else
